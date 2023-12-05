32c5ebd9dd994db40042b2d3aa1b736236dd22e6
liujia <redQueen@163.com>
2023-12-05 09:57:51 +0800

    引入pkg/errors，错误库

---
+++ test/error_test.go
@@ -0,0 +1,26 @@
+package test
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
+
+func TestError(t *testing.T) {
+	a := 10
+	b := 0
+	c, d := div(a, b)
+	if err, ok := d.(error); ok {
+		fmt.Println(err)
+	} else {
+		fmt.Println(c)
+	}
+}
+func div(a, b int) (int, error) {
+	if b == 0 {
+		err := errors.New("除数不为0")
+		return 0, err
+	} else {
+		return a / b, nil
+	}
+}
