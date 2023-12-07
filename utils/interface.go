9b448056f52cb98324f4c6bb1e98b7a23f9755c1
liujia <redQueen@163.com>
2023-12-07 16:01:37 +0800

    移除切片单个元素

--- utils/interface.go
+++ utils/interface.go
@@ -6,6 +6,7 @@ var check SliceInterface = &SortExistence{}
 
 type SliceInterface interface {
 	SliceIsExit(arr []string, one string) (index int, isExit bool)
+	SliceRemoveInt(int, []int) []int
 }
 type SortExistence struct {
 }
@@ -15,4 +16,15 @@ func (s *SortExistence) SliceIsExit(arr []string, one string) (index int, isExit
 	index = sort.SearchStrings(arr, one) //采用了二分查找，找到返回索引位置，没有找到返回可以插入的位置
 	isExit = index != len(arr) && arr[index] == one
 	return index, isExit
+}
+
+func (s *SortExistence) SliceRemoveInt(a int, arr []int) []int {
+	var index int
+	for k, v := range arr {
+		if v == a {
+			index = k
+		}
+	}
+	arr = append(arr[:index], arr[index+1:]...) //...表示将切片或数组拆开，作为可变参数传递给函数
+	return arr
 }
