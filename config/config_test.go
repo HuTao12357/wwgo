4a80e2461041f7fc5a13d3f2d7d3dffc86efea69
liujia <redQueen@163.com>
2023-12-11 10:12:57 +0800

    配置mysql的yml

---
+++ config/config_test.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"testing"
+)
+
+type User struct {
+	Id       int    `json:"id" form:"id"`
+	Username string `json:"username" form:"Username"`
+	Password string `json:"password" form:"password"`
+	Phone    string `json:"phone" form:"phone"`
+}
+
+func (u *User) TableName() string {
+	return "user"
+}
+func TestMysqlGet(t *testing.T) {
+	var user User
+	db, err := MysqlGet()
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.Where("id = ?", "3").First(&user)
+	fmt.Println("数据：", user)
+}
---
+++ config/mysqlConfig.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
+// Config 创建结构体解析配置信息
+type Config struct {
+	DB struct {
+		Host     string `yaml:"host"`
+		Port     string `yaml:"port"`
+		Name     string `yaml:"name"`
+		User     string `yaml:"user"`
+		Password string `yaml:"password"`
+	}
+}
+
+func GetMysqlConfig() (*Config, error) {
+	filepath := "mysql.yml"
+	file, err := os.ReadFile(filepath)
+	if err != nil {
+		log.Fatalf("fail to read config file: %v", err)
+		return nil, err
+	}
+	config := &Config{}
+	err = yaml.Unmarshal(file, config) //将yml数据解析目标结构体
+	if err != nil {
+		log.Fatalf("file to Unmarshal file :%v", err)
+		return nil, err
+	}
+	return config, nil
+}
+func MysqlGet() (*gorm.DB, error) {
+	config, err := GetMysqlConfig()
+	if err != nil {
+		log.Fatalf("fail to get config: %v", err)
+	}
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		config.DB.User,
+		config.DB.Password,
+		config.DB.Host,
+		config.DB.Port,
+		config.DB.Name,
+	)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("fail to connect database :%v", err)
+	}
+	return db, err
+}
